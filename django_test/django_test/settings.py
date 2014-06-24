dc7b5fcb3592a24defd5341520e4e98e5a6aa63f
jhgrng <jhgrng@gmail.com>
2014-06-24 13:21:15 +0200

    Added logging/registration

--- django_test/django_test/settings.py
+++ django_test/django_test/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'article',
-    'django_test',
+    'django_test'
 )
 
 MIDDLEWARE_CLASSES = (
--- django_test/django_test/views.py
+++ django_test/django_test/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
+from django.contrib.auth.forms import UserCreationForm
 
 
 #
@@ -41,3 +42,26 @@ def logout(request):
     auth.logout(request)
 
     return render_to_response('logout.html')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return render_to_response('register_success.html')
+            return HttpResponseRedirect('accounts/register_success')
+
+
+    args = {}
+    args.update(csrf(request))
+
+    args['form'] = UserCreationForm()
+
+    return render_to_response('register.html', args)
+
+
+def register_success(request):
+    return render_to_response('register_success.html')
